ac85d079e1fddab3b3d6f6bfbf7385eb07382bc9
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-13 10:39:46 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,24 @@
+import { get } from "http";
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+  // sinalizando que vai receber um repository
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+  async handle() {
+    try {
+      // validar requisição
+      // direcionar chamada para o repository
+      const users = await this.getUsersRepository.getUsers();
+
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Internal Server Error",
+      };
+    }
+  }
+}

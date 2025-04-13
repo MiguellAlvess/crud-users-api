282e51c4e1a6f0d2b4c8a778fbb4cafd44e88945
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-13 12:11:37 -0300

    feat: add id to User

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,14 +1,20 @@
+import { MongoClient } from "../../database/mongo";
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<User>("users")
+      .find()
+      .toArray();
     return [
       {
-        firstName: "Guilherme",
-        lastName: "Silva",
-        email: "a@b.com",
-        password: "123456",
+        id: users[0].id,
+        firstName: users[0].firstName,
+        lastName: users[0].lastName,
+        email: users[0].email,
+        password: users[0].password,
       },
     ];
   }

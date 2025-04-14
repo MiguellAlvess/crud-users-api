e99557ce00a1aba88e9684db65bcd44f6f852bc9
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-14 20:38:23 -0300

    feat: add CreateUserController with validations

---
+++ src/controllers/create-user/create-user.ts
@@ -0,0 +1,55 @@
+import validator from "validator";
+import { User } from "../../models/user";
+import { HttpRequest, HTTPResponse } from "../protocols";
+import {
+  CreateUserParams,
+  ICreateUserController,
+  ICreateUserRepository,
+} from "./protocols";
+
+export class CreateUserController implements ICreateUserController {
+  constructor(private readonly createUserRepository: ICreateUserRepository) {}
+  async handle(
+    httpRequest: HttpRequest<CreateUserParams>
+  ): Promise<HTTPResponse<User>> {
+    try {
+      const requiredFields = ["name", "email", "password"];
+
+      // verificando se todos os campos foram preenchidos
+
+      for (const field of requiredFields) {
+        if (!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
+          return {
+            statusCode: 400,
+            body: `O campo ${field} é obrigatório`,
+          };
+        }
+      }
+
+      // verificando se o email é valido
+
+      const emailIsValid = validator.isEmail(httpRequest.body!.email);
+
+      if (!emailIsValid) {
+        return {
+          statusCode: 400,
+          body: "O email é inválido",
+        };
+      }
+
+      const user = await this.createUserRepository.createUser(
+        httpRequest.body!
+      );
+
+      return {
+        statusCode: 201,
+        body: user,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Algo deu errado!",
+      };
+    }
+  }
+}
--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -2,3 +2,9 @@ export interface HTTPResponse<T> {
   statusCode: number;
   body: T | string;
 }
+
+export interface HttpRequest<B> {
+  params?: any;
+  headers?: any;
+  body?: B;
+}
